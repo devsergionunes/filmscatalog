78020c25784c4a6cca67d2ea9903c8449ded8d91
Paulo Sergio <devsergionunes@gmail.com>
2022-07-23 01:11:15 -0300

    material ui adicionado ao projeto, estilos default aplicados e theme criado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { ThemeProvider as ThemeMaterial, ThemeOptions } from "@mui/material";
+import { DefaultTheme, ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/GlobalStyles";
+import theme from "./styles/theme";
+
 function App() {
-  return <div>HELLO WORD</div>;
+  return (
+    <ThemeProvider theme={theme as DefaultTheme}>
+      <ThemeMaterial theme={theme as ThemeOptions}>
+        <div>HELO WORD</div>
+        <GlobalStyle />
+      </ThemeMaterial>
+    </ThemeProvider>
+  );
 }
 
 export default App;
